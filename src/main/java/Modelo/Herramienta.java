b8bdf0694ad1f3ef1aaa17f627e609ec40bcafab
Jonathan Medina <Usuario@Notebook-ASUS>
2022-11-23 16:29:40 -0300

    Agregado el paquete de Modelo / Clase Docente - Material - Herramienta

--- src/main/java/Modelo/Herramienta.java
+++ src/main/java/Modelo/Herramienta.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
-/**
- *
- * @author Usuario
- */
+
 public class Herramienta {
+    
+    private Integer idHerramienta;
+    private String nombre;
+    private String marca;
+    private Integer stock;
+    
+    public Herramienta (){
+        
+    }
+    
+    public Herramienta (Integer idHerramienta, String nombre, String marca, Integer stock){
+        this.idHerramienta = idHerramienta;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.stock = stock;
+    }
+
+    public Integer getIdHerramienta() {
+        return idHerramienta;
+    }
+
+    public void setIdHerramienta(Integer idHerramienta) {
+        this.idHerramienta = idHerramienta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Herramienta{" + "idHerramienta=" + idHerramienta + ", nombre=" + nombre + ", marca=" + marca + ", stock=" + stock + '}';
+    }
+    
     
 }
--- src/main/java/Modelo/Material.java
+++ src/main/java/Modelo/Material.java
@@ -4,4 +4,69 @@ package Modelo;
 
 public class Material {
     
+    private Integer idMaterial;
+    private String materiaPrima;
+    private String tipo;
+    private String medida;
+    private Integer stock;
+    
+    public Material(){
+        
+    }
+    
+    public Material (Integer idMaterial, String materiaPrima, String tipo, String medida, Integer stock){
+        
+        this.idMaterial = idMaterial;
+        this.materiaPrima = materiaPrima;
+        this.tipo = tipo;
+        this.medida = medida;
+        this.stock = stock;
+    }
+
+    public Integer getIdMaterial() {
+        return idMaterial;
+    }
+
+    public void setIdMaterial(Integer idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public String getMateriaPrima() {
+        return materiaPrima;
+    }
+
+    public void setMateriaPrima(String materiaPrima) {
+        this.materiaPrima = materiaPrima;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Material{" + "idMaterial=" + idMaterial + ", materiaPrima=" + materiaPrima + ", tipo=" + tipo + ", medida=" + medida + ", stock=" + stock + '}';
+    }
+    
+    
 }
