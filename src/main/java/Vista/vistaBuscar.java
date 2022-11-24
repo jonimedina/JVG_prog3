7697109e98e967b0039da0a774c5a4f19160f807
Jonathan Medina <Usuario@Notebook-ASUS>
2022-11-24 01:50:22 -0300

    COMPLETO PAQUETE MODELO Hibernate configurado

--- src/main/java/Vista/vistaBuscar.java
+++ src/main/java/Vista/vistaBuscar.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vista;
 
 /**
  *
- * @author Usuario
+ * @author Jonathan Medina
  */
 public class vistaBuscar extends javax.swing.JFrame {
 
