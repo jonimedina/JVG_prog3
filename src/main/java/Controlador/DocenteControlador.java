754869bf638fc211b01615b5c1b46209ef638c07
Jonathan Medina <Usuario@Notebook-ASUS>
2022-11-29 16:35:08 -0300

    Paquete Modelo: Completo Mapping por Entity completo Controladores EP

--- src/main/java/Controlador/DocenteControlador.java
+++ src/main/java/Controlador/DocenteControlador.java
@@ -1,8 +1,6 @@
 
 package Controlador;
 
-
-
 import Modelo.Docente;
 import java.util.List;
 import org.hibernate.Session;
@@ -31,5 +29,82 @@ public class DocenteControlador {
             return null;
         } 
     }
-    
+     
+     public Docente buscarDocentePorId(int id) {
+        try ( Session session = HibernateUtil.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Docente docenteEncontrado = session.createQuery("FROM Docente WHERE idDocente =:id", Docente.class).setParameter("id", id).getSingleResult();
+
+            session.getTransaction().commit();
+            if (docenteEncontrado != null) {
+                return docenteEncontrado;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } 
+    }
+
+    public Docente buscarDocentePorApellido(String ape) {
+        try ( Session session = HibernateUtil.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Docente docenteEncontrado = session.createQuery("FROM Docente WHERE apellido =:ape", Docente.class).setParameter("ape", ape).getSingleResult();
+
+            session.getTransaction().commit();
+            if (docenteEncontrado != null) {
+                return docenteEncontrado;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } 
+    }
+
+    public void actualizarDocente(Docente editarDocente) {
+        try ( Session session = HibernateUtil.getCurrentSession()) {
+
+            session.beginTransaction();
+            session.merge(editarDocente);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+    public Docente agregarDocente (Docente agregarDoc) {
+        try ( Session session = HibernateUtil.getCurrentSession()) {
+
+            session.beginTransaction();
+            session.persist(agregarDoc);
+            session.getTransaction().commit();
+            return agregarDoc;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void borrarDocente(int id) {
+        try ( Session session = HibernateUtil.getCurrentSession()) {
+
+            session.beginTransaction();
+            Docente eliminarDocente = session.createQuery("FROM Docente WHERE idDocente =:id", Docente.class).setParameter("id", id).getSingleResult();
+            session.remove(eliminarDocente);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+        
 }
